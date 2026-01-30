9d1b28753ae0ddc0356cff13cb5d00528a07f9bd
zhangwawa <kswangxing@hotmail.com>
2026-01-30 17:44:02 +0800

    Set tab and search options in options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.listchars = "tab:»■,trail:■"
+vim.opt.list = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.expandtab = false
